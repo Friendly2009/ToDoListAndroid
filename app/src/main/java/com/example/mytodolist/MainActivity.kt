43ae5f176cfc27129663a01dede71d5c366e084c
kirill <kirill.tsyganov@internet.ru>
2026-01-06 11:18:57 +0300

    сделан список из 20 записей, тест ui

--- app/src/main/java/com/example/mytodolist/MainActivity.kt
+++ app/src/main/java/com/example/mytodolist/MainActivity.kt
@@ -4,10 +4,34 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.rememberNavController
 import com.example.mytodolist.core.sqliteManager
 import com.example.mytodolist.ui.theme.MyToDoListTheme
 
@@ -21,11 +45,69 @@ class MainActivity : ComponentActivity() {
 
         val db = SqliteManager.readableDatabase
         setContent {
-            MyToDoListTheme(true) {
-                Surface(modifier = Modifier.fillMaxSize()) {
-                    Text(text = "Hello world, finally...")
+            MainScreen()
+        }
+    }
+}
+@Preview
+@Composable
+fun MainScreen(){
+    val itemsIndb = (1..20).toList()
+    MyToDoListTheme(true) {
+        Surface(modifier = Modifier.fillMaxSize()) {
+            Box(modifier = Modifier.fillMaxSize()){
+                LazyColumn(
+                    modifier = Modifier.fillMaxSize().padding(top = 80.dp),
+                    contentPadding = PaddingValues(16.dp),
+                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    items(itemsIndb){ item-> Subject()}
+                }
+                FloatingActionButton(
+                    onClick = { },
+                    modifier = Modifier
+                        .align(Alignment.BottomEnd)
+                        .padding(16.dp)
+                ) {
+                    Icon(imageVector = Icons.Filled.Add,"Добавить")
+                }
+                    FloatingActionButton(
+                        onClick = {},
+                        modifier = Modifier
+                            .align(Alignment.TopEnd)
+                            .padding(16.dp)
+                    ) {
+                        Icon(imageVector = Icons.Filled.Settings, "Настройки")
                 }
             }
         }
     }
 }
+@Composable
+fun Subject(){
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(4.dp)
+    ) {
+        Column(
+            modifier = Modifier
+                .padding(16.dp)
+        ) {
+            Text(
+                text = "Title",
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(
+                text = "description",
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onSurfaceVariant
+            )
+        }
+    }
+}
+
