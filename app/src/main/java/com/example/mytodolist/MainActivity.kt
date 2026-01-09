489296698a492fb79bdb77b06fd01a536119c8ee
kirill <kirill.tsyganov@internet.ru>
2026-01-09 14:45:39 +0300

    ListItem.kt перемещен в нужную папку

--- app/src/main/java/com/example/mytodolist/MainActivity.kt
+++ app/src/main/java/com/example/mytodolist/MainActivity.kt
@@ -7,7 +7,6 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
@@ -16,7 +15,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.mytodolist.core.SettingsViewModel
-import com.example.mytodolist.core.item
+import com.example.mytodolist.ui.screens.item
 import com.example.mytodolist.core.sqliteManager
 import com.example.mytodolist.ui.screens.MainScreen
 import com.example.mytodolist.ui.screens.Settings
--- app/src/main/java/com/example/mytodolist/ui/screens/ListItem.kt
+++ app/src/main/java/com/example/mytodolist/ui/screens/ListItem.kt
@@ -1,4 +1,4 @@
-package com.example.mytodolist.core
+package com.example.mytodolist.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
