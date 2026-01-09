58e6c8e1467a03f207109c7d8dd89fceb6f3b937
kirill <kirill.tsyganov@internet.ru>
2026-01-09 15:21:09 +0300

    добавление новой страницы

---
+++ app/src/main/java/com/example/mytodolist/ui/screens/devScreen.kt
@@ -0,0 +1,15 @@
+package com.example.mytodolist.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun devScreen(){
+    Column(modifier = Modifier.fillMaxSize().padding(16.dp)) {
+
+    }
+}
